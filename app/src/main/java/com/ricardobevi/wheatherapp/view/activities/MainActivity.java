2ee6dba543017c682004d22c718762f9e4dc3f07
Ricardo Bevilacqua <ricardo.bevilacqua@redmondsoftware.com>
2015-10-09 16:40:13 -0300

    Basic app skeleton

---
+++ app/src/main/java/com/ricardobevi/wheatherapp/view/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ricardobevi.wheatherapp.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ricardobevi.wheatherapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}

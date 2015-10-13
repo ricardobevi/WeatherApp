47efdf91b3a474e893669a334010ddf90d23ad71
Ricardo Bevilacqua <ricardob.redmond@gmail.com>
2015-10-12 21:53:45 -0300

    Add HttpHelper in order to get the raw string data and a little test to view it.

---
+++ app/src/main/java/com/ricardobevi/weatherapp/model/WeatherRequest.java
@@ -0,0 +1,11 @@
+package com.ricardobevi.weatherapp.model;
+
+
+/**
+ * Created by ric on 10/10/15.
+ */
+public class WeatherRequest {
+
+
+
+}
--- app/src/main/java/com/ricardobevi/weatherapp/view/fragments/MainActivityFragment.java
+++ app/src/main/java/com/ricardobevi/weatherapp/view/fragments/MainActivityFragment.java
@@ -5,20 +5,52 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ricardobevi.weatherapp.R;
+import com.ricardobevi.weatherapp.helper.HttpHelper;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    TextView textView;
+    HttpHelper httpHelper;
+
     public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        httpHelper = new HttpHelper( getString(R.string.infoUrl), getActivity().getApplicationContext());
+
+        httpHelper.setHttpHelperCallback( new HttpHelper.HttpHelperCallback() {
+            @Override
+            public void onDataAvailable(String data) {
+                setTextInfo(data);
+            }
+        });
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        textView = (TextView) view.findViewById(R.id.info_text);
+    }
+
+    private void setTextInfo(String text){
+        if ( textView != null )
+            textView.setText(text);
     }
 }
